e59fd1ef98647ca2a863dc3cf068e5fe051d3944
Rodrigo Torres Bergamaschi <143737837+RBergamaschi@users.noreply.github.com>
2025-06-13 10:22:45 -0300

    Update atividades.py

--- fast_zero/routers/atividades.py
+++ fast_zero/routers/atividades.py
@@ -25,10 +25,29 @@ def list_activities(
     db: Session = Depends(get_db),
     username: Optional[str] = None,
     nome_atividade: Optional[str] = None,
-    data: Optional[date] = Query(None)
+    data_inicial: Optional[date] = Query(None),
+    data_final: Optional[date] = Query(None)
 ):
-    return crud.pegar_atividades(db, username, nome_atividade, data)
+    if data_inicial and not data_final:
+        raise HTTPException(
+            status_code=400,
+            detail="Se data1 for fornecida, data2 também deve ser."
+        )
 
+    if data_inicial and data_final and data_final < data_inicial:
+        raise HTTPException(
+            status_code=400,
+            detail="data_final deve ser maior ou igual a data_inicial."
+        )
+    atividades = crud.pegar_atividades(db, username, nome_atividade, data_inicial, data_final)
+
+    if not atividades:
+        raise HTTPException(
+            status_code=404,
+            detail="Nenhuma atividade encontrada com os filtros fornecidos."
+        )
+
+    return atividades
 
 @router.patch('/{id_atividade}', response_model=schemas_atividades.Output_Atividade, description="Atualiza apenas os campos enviados")
 def update(id_atividade: int, atividade: schemas_atividades.AtualizarAtividade, db: Session = Depends(get_db)):
@@ -46,4 +65,11 @@ def delete(id_atividade: int, db: Session = Depends(get_db)):
 
 @router.get('/somatorio/{username}', response_model=schemas_atividades.SomatoriaAtividades)
 def summary(username: str, db: Session = Depends(get_db)):
-    return crud.pegar_somatoria(db, username)
+    resultado = crud.pegar_somatoria(db, username)
+
+    if not resultado:
+        raise HTTPException(
+            status_code=404,
+            detail='Usuário não encontrado ou sem atividades registradas.'
+        )
+    return resultado
